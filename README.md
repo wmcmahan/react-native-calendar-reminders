1d071747d95e28ecd8bc3e7659cbf6b58e5883d1
wmcmahan <mcmahanwill@gmail.com>
2016-01-14 15:27:02 -0600

    add and update reminder alarms

--- README.md
+++ README.md
@@ -31,31 +31,117 @@ RNCalendarReminders.authorizeEventStore((error, auth) => {...});
 RNCalendarReminders.fetchAllReminders(reminders => {...});
 ```
 
+#### Reminder Properties
+
+| Property        | Value            | Description |
+| --------------- |------------------| ----------- |
+| id              | String (read only)             | Unique id for the reminder. |
+| title           | String             | The title for the reminder. |
+| startDate       | Date             | The start date of the task. |
+| location        | String           | The location associated with the reminder. |
+| notes           | String           | The notes associated with the reminder. |
+| alarms          | Array            | The alarms associated with the reminder, as an array of alarm objects. |
 
 #### Create reminder
 
+```
+RNCalendarReminders.saveReminder(title, settings);
+```
+Example:
 ```javascript
-RNCalendarReminders.saveReminder(title, {
+RNCalendarReminders.saveReminder('title', {
   location: 'location',
   notes: 'notes',
   startDate: '2016-10-01T09:45:00.000UTC'
 });
 ```
 
+#### Create reminder with alarms
 
-#### Update reminder
-Give an **eventId** to update and existing reminder.
+###### Alarm options:
 
+| Property        | Value            | Description |
+| --------------- |------------------| ----------- |
+| date           | Date or Number    | If a Date is given, an alarm will be set with an absolute date. If a Number is given, an alarm will be set with a relative offset (in minutes) from the start date. |
+| location | Object             | The start date of the task. |
+
+###### Alarm location properties:
+
+| Property        | Value            | Description |
+| --------------- |------------------| ----------- |
+| title           | String  | The title of the location.|
+| proximity | String             | A value indicating how a location-based alarm is triggered. Possible values: `enter`, `leave`, `none`. |
+| radius | Number             | A minimum distance from the core location that would trigger the reminder's alarm. |
+| coords | Object             | The geolocation coordinates, as an object with latitude and longitude properties |
+
+Example with date:
 
 ```javascript
-RNCalendarReminders.saveReminder(title, {
-  id: eventId,
-  location: location,
-  notes: notes,
+RNCalendarReminders.saveReminder('title', {
+  location: 'location',
+  notes: 'notes',
+  startDate: '2016-10-01T09:45:00.000UTC',
+  alarms: [{
+    date: -1 // or absolute date
+  }]
+});
+```
+Example with location:
+
+```javascript
+RNCalendarReminders.saveReminder('title', {
+  location: 'location',
+  notes: 'notes',
+  startDate: '2016-10-01T09:45:00.000UTC',
+  alarms: [{
+    location: {
+      title: 'title',
+      proximity: 'enter',
+      radius: 500,
+      coords: {
+        latitude: 30.0000,
+        longitude: 97.0000
+      }
+    }
+  }]
+});
+```
+
+#### Update reminder
+Give an **reminder ID** to update and existing reminder.
+
+```javascript
+RNCalendarReminders.saveReminder('title', {
+  id: 'id',
+  location: 'location',
+  notes: 'notes',
   startDate: '2016-10-02T09:45:00.000UTC'
 });
 ```
 
+#### Update reminder alarms
+Give an **reminder ID** and **array of alarm options** to update and existing reminder. Note: This will overwrite any alarms already set on the reminder.
+
+```javascript
+RNCalendarReminders.addReminderAlarms({
+  id: 'id',
+  alarms: [{
+    date: -2 // or absolute date
+  }]
+});
+```
+
+#### Update reminder with added alarm
+Give an **reminder ID** and **alarm options object** to add new alarm.
+
+```javascript
+RNCalendarReminders.addReminderAlarm({
+  id: 'id',
+  alarms: {
+    date: -3 // or absolute date
+  }
+});
+```
 
 #### Remove reminder
 
--- RNCalendarReminders.m
+++ RNCalendarReminders.m
@@ -13,6 +13,7 @@ static NSString *const _title = @"title";
 static NSString *const _location = @"location";
 static NSString *const _startDate = @"startDate";
 static NSString *const _notes = @"notes";
+static NSString *const _alarms = @"alarms";
 
 @implementation RNCalendarReminders
 
@@ -81,38 +82,35 @@ RCT_EXPORT_MODULE()
           startDate:(NSDateComponents *)startDateComponents
            location:(NSString *)location
               notes:(NSString *)notes
+             alarms:(NSArray *)alarms
 {
-
     if (!self.isAccessToEventStoreGranted) {
         return;
     }
 
     EKReminder *reminder = [EKReminder reminderWithEventStore:self.eventStore];
+    reminder.calendar = [self.eventStore defaultCalendarForNewReminders];
     reminder.title = title;
     reminder.location = location;
-    reminder.startDateComponents = startDateComponents;
     reminder.dueDateComponents = startDateComponents;
+    reminder.startDateComponents = startDateComponents;
     reminder.completed = NO;
-    reminder.calendar = [self.eventStore defaultCalendarForNewReminders];
     reminder.notes = notes;
 
-    NSError *error = nil;
-    BOOL success = [self.eventStore saveReminder:reminder commit:YES error:&error];
-
-    if (!success) {
-        [self.bridge.eventDispatcher sendAppEventWithName:@"EventReminderError"
-                                                     body:@{@"error": @"Error saving reminder"}];
-    } else {
-        [self.bridge.eventDispatcher sendAppEventWithName:@"EventReminderSaved"
-                                                     body:reminder.calendarItemIdentifier];
+    if (alarms) {
+        reminder.alarms = [self createReminderAlarms:alarms];
     }
+
+    [self saveReminder:reminder];
 }
+
 
 - (void)editReminder:(EKReminder *)reminder
                title:(NSString *)title
            startDate:(NSDateComponents *)startDateComponents
             location:(NSString *)location
                notes:(NSString *)notes
+              alarms:(NSArray *)alarms
 {
     if (!self.isAccessToEventStoreGranted) {
         return;
@@ -125,6 +123,16 @@ RCT_EXPORT_MODULE()
     reminder.completed = NO;
     reminder.notes = notes;
 
+    if (alarms) {
+        reminder.alarms = [self createReminderAlarms:alarms];
+    }
+
+    [self saveReminder:reminder];
+}
+
+
+-(void)saveReminder:(EKReminder *)reminder
+{
     NSError *error = nil;
     BOOL success = [self.eventStore saveReminder:reminder commit:YES error:&error];
 
@@ -136,6 +144,7 @@ RCT_EXPORT_MODULE()
                                                      body:reminder.calendarItemIdentifier];
     }
 }
+
 
 - (void)deleteReminder:(NSString *)eventId
 {
@@ -144,7 +153,6 @@ RCT_EXPORT_MODULE()
     }
 
     EKReminder *reminder = (EKReminder *)[self.eventStore calendarItemWithIdentifier:eventId];
-
     NSError *error = nil;
     BOOL success = [self.eventStore removeReminder:reminder commit:YES error:&error];
 
@@ -152,25 +160,112 @@ RCT_EXPORT_MODULE()
         [self.bridge.eventDispatcher sendAppEventWithName:@"EventReminderError"
                                                      body:@{@"error": @"Error removing reminder"}];
     }
-
 }
+
+#pragma mark -
+#pragma mark Alarms
+
+
+- (EKAlarm *)createReminderAlarm:(NSDictionary *)alarm
+{
+    EKAlarm *reminderAlarm = nil;
+    id alarmDate = [alarm valueForKey:@"date"];
+
+    if ([alarmDate isKindOfClass:[NSString class]]) {
+        reminderAlarm = [EKAlarm alarmWithAbsoluteDate:[RCTConvert NSDate:alarmDate]];
+    } else if ([alarmDate isKindOfClass:[NSNumber class]]) {
+        int minutes = [alarmDate intValue];
+        reminderAlarm = [EKAlarm alarmWithRelativeOffset:(60 * minutes)];
+    } else {
+        reminderAlarm = [[EKAlarm alloc] init];
+    }
+
+    if ([alarm objectForKey:@"location"] && [[alarm objectForKey:@"location"] count]) {
+        NSDictionary *locationOptions = [alarm valueForKey:@"location"];
+        NSDictionary *geo = [locationOptions valueForKey:@"coords"];
+        CLLocation *geoLocation = [[CLLocation alloc] initWithLatitude:[[geo valueForKey:@"latitude"] doubleValue]
+                                                             longitude:[[geo valueForKey:@"longitude"] doubleValue]];
+
+        reminderAlarm.structuredLocation = [EKStructuredLocation locationWithTitle:[locationOptions valueForKey:@"title"]];
+        reminderAlarm.structuredLocation.geoLocation = geoLocation;
+        reminderAlarm.structuredLocation.radius = [[locationOptions valueForKey:@"radius"] doubleValue];
+
+        if ([[locationOptions valueForKey:@"proximity"] isEqualToString:@"enter"]) {
+            reminderAlarm.proximity = EKAlarmProximityEnter;
+        } else if ([[locationOptions valueForKey:@"proximity"] isEqualToString:@"leave"]) {
+            reminderAlarm.proximity = EKAlarmProximityLeave;
+        } else {
+            reminderAlarm.proximity = EKAlarmProximityNone;
+        }
+    }
+    return reminderAlarm;
+}
+
+
+- (NSArray *)createReminderAlarms:(NSArray *)alarms
+{
+    NSMutableArray *reminderAlarms = [[NSMutableArray alloc] init];
+    for (NSDictionary *alarm in alarms) {
+        if ([alarm count] && ([alarm valueForKey:@"date"] || [alarm objectForKey:@"location"])) {
+            EKAlarm *reminderAlarm = [self createReminderAlarm:alarm];
+            [reminderAlarms addObject:reminderAlarm];
+        }
+    }
+    return [reminderAlarms copy];
+}
+
+
+- (void)addReminderAlarm:(NSString *)eventId alarm:(NSDictionary *)alarm
+{
+    if (!self.isAccessToEventStoreGranted) {
+        return;
+    }
+
+    EKReminder *reminder = (EKReminder *)[self.eventStore calendarItemWithIdentifier:eventId];
+    EKAlarm *reminderAlarm = [self createReminderAlarm:alarm];
+    [reminder addAlarm:reminderAlarm];
+
+    [self saveReminder:reminder];
+}
+
+
+- (void)addReminderAlarms:(NSString *)eventId alarms:(NSArray *)alarms
+{
+    if (!self.isAccessToEventStoreGranted) {
+        return;
+    }
+
+    EKReminder *reminder = (EKReminder *)[self.eventStore calendarItemWithIdentifier:eventId];
+    reminder.alarms = [self createReminderAlarms:alarms];
+
+    [self saveReminder:reminder];
+}
+
+#pragma mark -
+#pragma mark Serializers
 
 - (NSArray *)serializeReminders:(NSArray *)reminders
 {
-    static NSString *const dateFormat = @"yyyy-MM-dd'T'HH:mm:ss.SSS'Z";
-
     NSMutableArray *serializedReminders = [[NSMutableArray alloc] init];
 
-    NSDictionary *empty_reminder = @{
-        _title: @"",
-        _location: @"",
-        _startDate: @"",
-        _notes: @"",
-    };
+    NSDictionary *emptyReminder = @{
+                                    _title: @"",
+                                    _location: @"",
+                                    _startDate: @"",
+                                    _notes: @"",
+                                    _alarms: @[]
+                                    };
+
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"UTC"];
+    [dateFormatter setTimeZone:timeZone];
+    [dateFormatter setLocale:[NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"]];
+    [dateFormatter setDateFormat: @"yyyy-MM-dd'T'HH:mm:ss.SSS'Z"];
 
     for (EKReminder *reminder in reminders) {
 
-        NSMutableDictionary *formedReminder = [NSMutableDictionary dictionaryWithDictionary:empty_reminder];
+        NSMutableDictionary *formedReminder = [NSMutableDictionary dictionaryWithDictionary:emptyReminder];
 
         if (reminder.calendarItemIdentifier) {
             [formedReminder setValue:reminder.calendarItemIdentifier forKey:_id];
@@ -181,37 +276,77 @@ RCT_EXPORT_MODULE()
         }
 
         if (reminder.notes) {
-          [formedReminder setValue:reminder.notes forKey:_notes];
+            [formedReminder setValue:reminder.notes forKey:_notes];
         }
 
         if (reminder.location) {
             [formedReminder setValue:reminder.location forKey:_location];
         }
 
+        if (reminder.hasAlarms) {
+            NSMutableArray *alarms = [[NSMutableArray alloc] init];
+
+            for (EKAlarm *alarm in reminder.alarms) {
+                NSMutableDictionary *formattedAlarm = [[NSMutableDictionary alloc] init];
+                NSString *alarmDate = nil;
+
+                if (alarm.absoluteDate) {
+                    alarmDate = [dateFormatter stringFromDate:alarm.absoluteDate];
+                } else if (alarm.relativeOffset) {
+                    NSDate *reminderStartDate = nil;
+                    if (reminder.startDateComponents) {
+                        reminderStartDate = [calendar dateFromComponents:reminder.startDateComponents];
+                    } else {
+                        reminderStartDate = [NSDate date];
+                    }
+                    alarmDate = [dateFormatter stringFromDate:[NSDate dateWithTimeInterval:alarm.relativeOffset
+                                                                                 sinceDate:reminderStartDate]];
+                }
+                [formattedAlarm setValue:alarmDate forKey:@"date"];
+
+                if (alarm.structuredLocation) {
+                    NSString *proximity = nil;
+                    switch (alarm.proximity) {
+                        case EKAlarmProximityEnter:
+                            proximity = @"enter";
+                            break;
+                        case EKAlarmProximityLeave:
+                            proximity = @"leave";
+                            break;
+                        default:
+                            proximity = @"None";
+                            break;
+                    }
+                    [formattedAlarm setValue:@{
+                                               @"title": alarm.structuredLocation.title,
+                                               @"proximity": proximity,
+                                               @"radius": @(alarm.structuredLocation.radius),
+                                               @"coords": @{
+                                                       @"latitude": @(alarm.structuredLocation.geoLocation.coordinate.latitude),
+                                                       @"longitude": @(alarm.structuredLocation.geoLocation.coordinate.longitude)
+                                                       }}
+                                      forKey:@"location"];
+
+                }
+                [alarms addObject:formattedAlarm];
+            }
+            [formedReminder setValue:alarms forKey:_alarms];
+        }
+
         if (reminder.startDateComponents) {
-            NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-
-            NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"UTC"];
-
-            [dateFormatter setTimeZone:timeZone];
-            [dateFormatter setDateFormat:dateFormat];
-
             NSDate *reminderStartDate = [calendar dateFromComponents:reminder.startDateComponents];
-
             [formedReminder setValue:[dateFormatter stringFromDate:reminderStartDate] forKey:_startDate];
         }
 
         [serializedReminders addObject:formedReminder];
     }
 
-    NSArray *remindersCopy = [serializedReminders copy];
-
-    return remindersCopy;
+    return [serializedReminders copy];
 }
 
 #pragma mark -
 #pragma mark notifications
+
 
 - (void)addNotificationCenter
 {
@@ -221,10 +356,6 @@ RCT_EXPORT_MODULE()
                                                object:nil];
 }
 
-- (void) dealloc
-{
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
 
 - (void)calendarEventReminderReceived:(NSNotification *)notification
 {
@@ -237,7 +368,11 @@ RCT_EXPORT_MODULE()
                                                              body:[weakSelf serializeReminders:reminders]];
         });
     }];
+}
 
+- (void) dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 #pragma mark -
@@ -268,6 +403,7 @@ RCT_EXPORT_METHOD(saveReminder:(NSString *)title details:(NSDictionary *)details
     NSString *location = [RCTConvert NSString:details[_location]];
     NSDate *startDate = [RCTConvert NSDate:details[_startDate]];
     NSString *notes = [RCTConvert NSString:details[_notes]];
+    NSArray *alarms = [RCTConvert NSArray:details[_alarms]];
 
     NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
     NSDateComponents *startDateComponents = [gregorianCalendar components:(NSCalendarUnitEra | NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit)
@@ -275,16 +411,35 @@ RCT_EXPORT_METHOD(saveReminder:(NSString *)title details:(NSDictionary *)details
 
     if (eventId) {
         EKReminder *reminder = (EKReminder *)[self.eventStore calendarItemWithIdentifier:eventId];
-        [self editReminder:reminder title:title startDate:startDateComponents location:location notes:notes];
+        [self editReminder:reminder
+                     title:title
+                 startDate:startDateComponents
+                  location:location
+                     notes:notes
+                    alarms:alarms];
 
     } else {
-        [self addReminder:title startDate:startDateComponents location:location notes:notes];
+        [self addReminder:title
+                startDate:startDateComponents
+                 location:location
+                    notes:notes
+                   alarms:alarms];
     }
 }
 
 RCT_EXPORT_METHOD(removeReminder:(NSString *)eventId)
 {
-    [self deleteReminder:(NSString *)eventId];
+    [self deleteReminder:eventId];
+}
+
+RCT_EXPORT_METHOD(addAlarm:(NSString *)eventId alarm:(NSDictionary *)alarm)
+{
+    [self addReminderAlarm:eventId alarm:alarm];
+}
+
+RCT_EXPORT_METHOD(addAlarms:(NSString *)eventId alarms:(NSArray *)alarms)
+{
+    [self addReminderAlarms:eventId alarms:alarms];
 }
 
 @end
